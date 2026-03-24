2e16744ddff3e81bad68210a74113fe7c1552b73
Nitin Negi <nitinnegi@Nitins-MacBook-Air.local>
2026-03-24 18:21:05 +0530

    Defer FontAwesome stylesheet loading to fix LCP regression

--- app/layout.tsx
+++ app/layout.tsx
@@ -87,7 +87,15 @@ export default function RootLayout({
         <html lang="en" className={`${outfit.variable} ${inter.variable}`}>
             <head>
                 <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/webfonts/fa-solid-900.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+                <link 
+                    rel="stylesheet" 
+                    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
+                    media="print"
+                    onLoad={(e) => { e.currentTarget.media = 'all'; }}
+                />
+                <noscript>
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+                </noscript>
                 <script
                     type="application/ld+json"
                     dangerouslySetInnerHTML={{ __html: JSON.stringify([organizationSchema, websiteSchema]) }}
